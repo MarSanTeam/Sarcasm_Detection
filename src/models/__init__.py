4c3b35ff48059e30330c3fe4a7d137dbdd0d3381
ehsan.tavan <ehsan.tavan@partsoftware.com>
2022-07-09 15:57:08 +0430

    [add] add docstring

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,8 @@
-from .sar_mt5_transformer_dc import Classifier
+# -*- coding: utf-8 -*-
+"""
+    Sarcasm Detection Project:
+        Make the importing much shorter
+"""
+
 from .helper import build_checkpoint_callback
 from .dataset import DataModule
